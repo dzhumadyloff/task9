9f5b0b43a5f587c3dc6c0595ff77f67de48af72f
dzhumadyloff <80409251+dzhumadyloff@users.noreply.github.com>
2021-05-02 21:13:20 +0600

    Add files via upload

---
+++ week_days_model.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class WeekDaysModel {
+  final String day;
+  final String degree;
+  final IconData icon;
+
+  WeekDaysModel({this.day, this.degree, this.icon});
+}
